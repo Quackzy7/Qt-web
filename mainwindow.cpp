f6bf5475f928fed6f9a21d24f13b3458d699680b
Quackzy7 <abhishu.panthi@gmail.com>
2024-08-24 09:14:28 +0545

    3

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,11 +16,15 @@ MainWindow::MainWindow(QWidget *parent)
     QWebEngineView *webView = ui->webEngineView;
 
     // Load a default webpage
-    webView->load(QUrl("https://www.qt.io"));
+    webView->load(QUrl("https://www.google.com"));
 
     // Connect buttons to webView actions
     connect(backButton, &QPushButton::clicked, webView, &QWebEngineView::back);
     connect(forwardButton, &QPushButton::clicked, webView, &QWebEngineView::forward);
+
+    // Connect the returnPressed signal of the QLineEdit to the slot
+    connect(ui->lineEdit, &QLineEdit::returnPressed, this, &MainWindow::onUrlEntered);
+
 }
 
 
@@ -28,3 +32,18 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::onUrlEntered()
+{
+    QString urlText = ui->lineEdit->text();
+
+    // Basic check to see if the input is a URL
+    if (!urlText.startsWith("http://") && !urlText.startsWith("https://")) {
+        // Treat it as a search query if it's not a URL
+        urlText = "https://www.google.com/search?q=" + urlText;
+    }
+
+    QUrl url = QUrl::fromUserInput(urlText);
+    ui->webEngineView->setUrl(url);
+}
+
